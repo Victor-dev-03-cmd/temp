db731454f2e6d18293e7de2a287c446178910e40
Victor-dev-03-cmd <developerconsole03@gmail.com>
2026-01-23 12:31:33 +0530

    commited

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaMySQL } from "@prisma/adapter-mysql"
+import { createPool } from "mysql2"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  })
+const pool = createPool({
+  uri: process.env.DATABASE_URL,
+})
+const adapter = new PrismaMySQL(pool)
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
